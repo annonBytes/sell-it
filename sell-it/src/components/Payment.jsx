e106b87f587d6d99a7efbc6f7013a4b5ba00d5fa
Daniel Ockiya <annonbytes@gmail.com>
2021-02-15 21:16:40 +0100

    Payment Page Frontend

--- sell-it/src/components/Payment.jsx
+++ sell-it/src/components/Payment.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import '../components/Payment.css';
 import { useStateValue } from '../StateProvider';
+import CheckoutProduct from './CheckoutProduct';
 
 function Payment() {
 
@@ -9,6 +11,7 @@ function Payment() {
     return (
         <div className="payment">
             <div className="payment__container">
+                <h1>Checkout (<Link to="/checkout">{basket?.length} items</Link>)</h1>
                 <div className="payment__section">
                       <div className="payment__title">
                           <h3>Delivery Address</h3>
@@ -21,11 +24,32 @@ function Payment() {
                 </div>
 
                 <div className="payment__section">
-                    
+                     <div className="payment__title">
+                          <h3>Review Items and Delivery</h3>
+                      </div>
+
+                      <div className="payment__items">
+                          {basket.map(
+                              item => (
+                                  <CheckoutProduct 
+                                      id = {item.id}
+                                      title = {item.title}
+                                      image = {item.image}
+                                      price = {item.price}
+                                      rating = {item.rating}
+                                  />
+                              )
+                          )}
+                      </div>
                 </div>
 
                 <div className="payment__section">
-                    
+                    <div className="payment__title">
+                          <h3>Payment Method</h3>
+                      </div>
+                      <div className="payment__details">
+
+                      </div>
                 </div>
             </div>
         </div>
